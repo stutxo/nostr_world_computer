d81b0171a992a33175885f87d2e70267a7aef04e
stutxo <70952638+stutxo@users.noreply.github.com>
2024-04-11 17:38:55 -0500

    remove no std stuff for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
-// #![no_std]
-
-// #[panic_handler]
-// fn handle_panic(_: &core::panic::PanicInfo) -> ! {
-//     loop {}
-// }
-
 #[no_mangle]
 pub extern "C" fn world_computer() -> u32 {
     42
